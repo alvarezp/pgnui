76832a07e639a565e6e1e4d3917ddc0670c34697
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2009-10-18 16:36:28 -0700

    Remove explicit NULL support in GUI in favor of NULL when zero-length string.

--- src/html_control_textbox.php
+++ src/html_control_textbox.php
@@ -29,20 +29,13 @@ class HtmlControlTextbox extends HtmlControl {
 	}
 
 	public function get_sql_update_from_diff($bef, $aft) {
-		if ($bef[known] != $aft[known]) {
-			if ($aft[known] == 'no') {
+		if ($bef[value] != $aft[value]) {
+			if ($aft[value] == '') {
 				return array(change => 'yes', value => NULL);
-			}
-			if ($aft[known] == 'yes') {
+			} else {
 				return array(change => 'yes', value => $aft[value]);
 			}
 		}
-		if ($aft[known] == 'yes') {
-			if ($bef[value] != $aft[value]) {
-				return array(change => 'yes', value => $aft[value]);
-			}
-		}
-		return array(change => 'no', value => '');
 	}
 
 	public function get_html_editable($basename) {
@@ -51,18 +44,10 @@ class HtmlControlTextbox extends HtmlControl {
 		$r .= "<span class='textbox' id='${basename}'>\n";
 
 		if (isset($this->value)) {
-			$r .= "<input type='hidden' name='${basename}[bef][known]' value='yes'></input>\n";
 			$r .= "<input type='hidden' name='${basename}[bef][value]' value='$this->value'></input>\n";
-			$r .= "<input id='${basename}[aft][known]=no' type='radio' name='${basename}[aft][known]' class='rad_unknown' value='no'></input>\n";
-			$r .= "<label class='rad_unknown' for='_yes_isknown_${basename}'>Not set</label><br/>\n";
-			$r .= "<input id='${basename}[aft][known]=yes' type='radio' name='${basename}[aft][known]' class='rad_known' value='yes' checked='checked'></input>\n";
 			$r .= "<input id='${basename}[aft][value]' type='text' name='${basename}[aft][value]' class='textbox' value='$this->value'></input>\n";
 		} else {
-			$r .= "<input type='hidden' name='${basename}[bef][known]' value='no'></input>\n";
-			$r .= "<input type='hidden' name='${basename}[bef][value]' value='$this->value'></input>\n";
-			$r .= "<input id='${basename}[aft][known]=no' type='radio' name='${basename}[aft][known]' class='rad_unknown' value='no' checked='checked'></input>\n";
-			$r .= "<label class='rad_unknown' for='_yes_isknown_${basename}'>Not set</label><br/>\n";
-			$r .= "<input id='${basename}[aft][known]=yes' type='radio' name='${basename}[aft][known]' class='rad_known' value='yes'></input>\n";
+			$r .= "<input type='hidden' name='${basename}[bef][value]' value=''></input>\n";
 			$r .= "<input id='${basename}[aft][value]' type='text' name='${basename}[aft][value]' class='textbox' value=''></input>\n";
 		}
 		$r .= "</span>";
