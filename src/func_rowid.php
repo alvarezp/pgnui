774776b512dbb62b5ff6b60cfe039e1607d3f19d
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2009-10-18 19:19:25 -0700

    Fixed rowid management. It was messed up.

--- src/func_rowid.php
+++ src/func_rowid.php
@@ -5,8 +5,8 @@ function record_columns_to_rowid($table_columns, $r) {
 	$cid = 0;
 	foreach($r as $col_name => $d):
 		if ($table_columns[$cid][position_in_pk] > 0) {
-			if ($rowid != "") $rowid .= "&";
-			$rowid .= urlencode(urlencode($col_name)) . "=" . urlencode(urlencode($d)) . "";
+			if ($rowid != "") $rowid .= urlencode("&");
+			$rowid .= urlencode(urlencode($col_name) . "=" . urlencode($d)) . "";
 		}
 		$cid++;
 	endforeach;
@@ -20,9 +20,8 @@ function rowid_to_record_columns($rowid) {
 
 function rowid_to_where($rowid) {
 	$string = urldecode($rowid);
+	$string = preg_replace ("/=([^&]*)/", "='$1'", $string);
 	$string = preg_replace ("/&/", " AND ", $string);
-	$string = preg_replace ("/=(.*) AND/", "='$1' AND", $string);
-	$string = preg_replace ("/=(.*)$/", "='$1'", $string);
 	$string = urldecode($string);
 
 	return $string;
