54672a746f15ddd7147036eef2c80ae24a810528
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2009-11-01 19:13:03 -0800

    Removed typo in SQL DDL statement.

--- src/sql/020-___pgnui_functions.sql
+++ src/sql/020-___pgnui_functions.sql
@@ -1,7 +1,7 @@
 DROP SCHEMA ___pgnui_functions CASCADE;
 CREATE SCHEMA ___pgnui_functions;
 
-DROP FUNCTION ___pgnui_functions.if_table_exists(_catalog, varchar, _schema varchar, _table varchar);
+DROP FUNCTION ___pgnui_functions.if_table_exists(_catalog varchar, _schema varchar, _table varchar);
 CREATE FUNCTION ___pgnui_functions.if_table_exists(varchar, varchar, varchar) RETURNS BOOLEAN AS $$
 	SELECT count(*) > 0 FROM information_schema.tables WHERE table_catalog = $1 AND table_schema = $2 AND table_name = $3;
 $$ LANGUAGE SQL;
