de491de0a0a9c077ae2fc4813ea5839d695014e9
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2014-06-13 01:15:31 -0700

    Let the "date" data type be handled by a "date" HTML control

--- src/interactions/table/record-read.php
+++ src/interactions/table/record-read.php
@@ -35,6 +35,9 @@ function get_columns_as_keys($dbconn, $catalog, $schema, $table) {
 			} else if ($field['data_type'] == "character varying") {
 				require_once("html_control_textbox.php");
 				$pretty_columns[$field['column_name']]['control'] = new HtmlControlTextbox;
+			} else if ($field['data_type'] == "date") {
+				require_once("html_control_calendar.php");
+				$pretty_columns[$field['column_name']]['control'] = new HtmlControlCalendar;
 			} else if ($field['data_type'] == "USER-DEFINED" && $field['udt_name'] == "cryptmd5") {
 				require_once("html_control_password.php");
 				$pretty_columns[$field['column_name']]['control'] = new HtmlControlPassword;
